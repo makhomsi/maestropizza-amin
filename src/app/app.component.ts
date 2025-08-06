bb85b3ccc247dab3fcc5896318c67ebe62cdeb58
Amin <makhomsi@pav-online.com>
2025-08-06 08:30:17 +0200

    feat: add app -> component receta style decorado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,],
   template: `
     
     <router-outlet></router-outlet>
